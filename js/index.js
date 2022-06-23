ce70aa054075ab9819531dd356346f1f4b6c98df
denzkie18 <superdenz@gmail.com>
2022-06-23 16:49:27 +0800

    Added New LN

--- js/index.js
+++ js/index.js
@@ -3,6 +3,10 @@ $(document).ready(function(){
   //List of Novels
   let novel_index = [
       {
+        title: "[New Life+] Young Again in Another World",
+        sourceLink: "series/Young Again in Another World-Light-Novel.html"
+      },
+      {
         title: "Date A Live",
         sourceLink: "series/Date A Live-Light-Novel.html"
       },
